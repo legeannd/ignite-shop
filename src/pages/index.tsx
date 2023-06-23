3247af2838c33240d4d37d8ab9d139c305549ba6
Gean Lucas <gean.lfpsa@gmail.com>
2023-06-22 21:15:36 -0300

    add stripe initial config

---
+++ src/pages/index.tsx
@@ -0,0 +1,52 @@
+import 'keen-slider/keen-slider.min.css'
+
+import Image from "next/image"
+
+import { useKeenSlider } from 'keen-slider/react'
+
+import { HomeContainer, Product } from "../styles/pages/home"
+
+export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
+}
