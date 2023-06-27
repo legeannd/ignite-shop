b9f8fab54cee0c341a82a946e9a4e4bd18b58144
Gean Lucas <gean.lfpsa@gmail.com>
2023-06-26 21:49:00 -0300

    create stripe checkout api handler

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -11,10 +11,15 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   }
 }
 
 export default function Product({ product }: ProductProps) {
+  function handleBuyProduct() {
+    console.log(product.defaultPriceId)
+  }
+
   return (
     <ProductContainer>
       <ImageContainer>
@@ -28,7 +33,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>Comprar agora</button>
+        <button onClick={handleBuyProduct} >Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
@@ -63,7 +68,8 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           currency: 'BRL'
         }).format(price.unit_amount / 100),
         description: product.description,
-      },
+        defaultPriceId: price.id,
+      }
     },
     revalidate: 60 * 60 * 1, // 1 hour
   }
